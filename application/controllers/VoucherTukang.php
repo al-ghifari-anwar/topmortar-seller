09790a2bc4eedbe6b98527cd1271bf2c94f40eaa
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-08-14 15:00:05 +0700

    API claim voucher

--- application/controllers/VoucherTukang.php
+++ application/controllers/VoucherTukang.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class RekeningToko extends CI_Controller
+class VoucherTukang extends CI_Controller
 {
     public function __construct()
     {
