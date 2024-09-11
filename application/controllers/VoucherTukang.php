fedc8066d0ea5863a7ce9236c1bf6c817e3b8359
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-09-11 14:33:58 +0700

    Penukaran toko promo

--- application/controllers/VoucherTukang.php
+++ application/controllers/VoucherTukang.php
@@ -137,7 +137,8 @@ class VoucherTukang extends CI_Controller
                             $result = [
                                 'code' => 400,
                                 'status' => 'failed',
-                                'msg' => 'Proses claim gagal'
+                                'msg' => 'Proses claim gagal',
+                                'detail' => $res['detail']
                             ];
 
                             $this->output->set_output(json_encode($result));
