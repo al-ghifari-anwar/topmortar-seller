0bec25718fe0c7986e0d2428d8edc8ba5870cea9
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-07-27 09:11:59 +0700

    Apporder satuan

--- application/models/MApporderDetail.php
+++ application/models/MApporderDetail.php
@@ -4,6 +4,8 @@ class MApporderDetail extends CI_Model
 {
     public function getByIdApporder($id_apporder)
     {
+        $this->db->join('tb_produk', 'tb_produk.id_produk = tb_apporder_detail.id_produk');
+        $this->db->join('tb_satuan', 'tb_satuan.id_satuan = tb_produk.id_satuan');
         $query = $this->db->get_where('tb_apporder_detail', ['id_apporder' => $id_apporder])->result_array();
 
         return $query;
@@ -11,6 +13,8 @@ class MApporderDetail extends CI_Model
 
     public function getByIdApporderLimit($id_apporder)
     {
+        $this->db->join('tb_produk', 'tb_produk.id_produk = tb_apporder_detail.id_produk');
+        $this->db->join('tb_satuan', 'tb_satuan.id_satuan = tb_produk.id_satuan');
         $query = $this->db->get_where('tb_apporder_detail', ['id_apporder' => $id_apporder], 2)->result_array();
 
         return $query;
