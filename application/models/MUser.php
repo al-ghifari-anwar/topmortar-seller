c9d18096d2bbf08d4345f8bef4d3d918aa73197d
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-09-17 12:40:12 +0700

    Kurir auto city

--- application/models/MUser.php
+++ application/models/MUser.php
@@ -5,7 +5,7 @@ class MUser extends CI_Model
 {
     public function getCourierByIdCity($id_city)
     {
-        $query = $this->db->get_where('tb_courier', ['id_city' => $id_city])->row_array();
+        $query = $this->db->get_where('tb_user', ['id_city' => $id_city, 'level_user' => 'courier'])->row_array();
 
         return $query;
     }
